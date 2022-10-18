c3959647b962786ad29da06f3fe4107d9a8ad633
P1atyphy11a <lhy010108@outlook.com>
2022-10-18 05:01:58 -0400

    Updated Result Display to add tips (single result)

--- Education_Pathways/frontend/src/components/ResultDisplay.jsx
+++ Education_Pathways/frontend/src/components/ResultDisplay.jsx
@@ -37,7 +37,7 @@ class SearchResultDisplay extends Component{
           if (res.data.length > 0) {
             let len = res.data.length
             let result_temp = []
-            result_temp.push(<h>Click on course code to go to course detail page</h>)
+            result_temp.push(<h>Tips: Click on course code to go to course detail page</h>)
             result_temp.push(<Label></Label>)
             for (let i = 0; i < len; i++) {
                 result_temp.push(<Result course_code={res.data[i].code} course_name={res.data[i].name}></Result>)
@@ -47,6 +47,7 @@ class SearchResultDisplay extends Component{
             alert("Course not found")
           }else {
             let result_temp = []
+            result_temp.push(<h>Tips: Click on course code to go to course detail page</h>)
             result_temp.push(<Label></Label>)
             result_temp.push(<Result course_code={res.data.course.code} course_name={res.data.course.name}></Result>)
             this.setState({results: result_temp})
