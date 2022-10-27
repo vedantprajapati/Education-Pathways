c711ff4ea75c65386a4041f97e721b3539ffe6b6
P1atyphy11a <lhy010108@outlook.com>
2022-10-26 21:14:53 -0400

    styling to match homepage

--- Education_Pathways/frontend/src/components/AddPathway.js
+++ Education_Pathways/frontend/src/components/AddPathway.js
@@ -97,47 +97,47 @@ class AddPathwayPage extends Component {
                 <Container className="pathway-form">
                     <Row float="center" className="form-title">
                         <Col>
-                            <h1>Add New Pathway</h1>
+                            <h1 style={{marginTop:"30px"}}>Add New Pathway</h1>
                         </Col>
                     </Row>
-                        <form className="form" onSubmit={this.handleSubmit} style={{borderRadius:"20px", borderColor:"00008b", width:"auto", textAlign:"center"}}>
-                            <div>Pathway Title<input type={"text"} ref="title" placeholder="Pathway Title" style={{width:"600px"}} /></div>
+                        <form className="form" onSubmit={this.handleSubmit} style={{textAlign:"center" ,borderRadius: "15px", border: "2px solid #c4c4c4"}}>
+                            <div>Pathway Title<input type={"text"} ref="title" placeholder="Pathway Title" style={{width:"600px", borderRadius: "5px", border: "2px solid #c4c4c4"}} /></div>
                             <div>
                                 Y3 H1
-                                <input name="course1" type={"text"} ref="course1" placeholder="course1" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course2" type={"text"} ref="course2" placeholder="course2" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course3" type={"text"} ref="course3" placeholder="course3" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course4" type={"text"} ref="course4" placeholder="course4" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course5" type={"text"} ref="course5" placeholder="course5" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course6" type={"text"} ref="course6" placeholder="course6" style={{width:"100px", margin:"5px"}}/>
+                                <input name="course1" type={"text"} ref="course1" placeholder="course1" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course2" type={"text"} ref="course2" placeholder="course2" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course3" type={"text"} ref="course3" placeholder="course3" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course4" type={"text"} ref="course4" placeholder="course4" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course5" type={"text"} ref="course5" placeholder="course5" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course6" type={"text"} ref="course6" placeholder="course6" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
                             </div><div>
                                 Y3 H2
-                                <input nameref="course7"  type={"text"} ref="course7" placeholder="course7" style={{width:"100px", margin:"5px"}}/>
-                                <input nameref="course8"  type={"text"} ref="course8" placeholder="course8" style={{width:"100px", margin:"5px"}}/>
-                                <input nameref="course9"  type={"text"} ref="course9" placeholder="course9" style={{width:"100px", margin:"5px"}}/>
-                                <input nameref="course10" type={"text"} ref="course10" placeholder="course10" style={{width:"100px", margin:"5px"}}/>
-                                <input nameref="course11" type={"text"} ref="course11" placeholder="course11" style={{width:"100px", margin:"5px"}}/>
-                                <input nameref="course12" type={"text"} ref="course12" placeholder="course12" style={{width:"100px", margin:"5px"}}/>
+                                <input nameref="course7"  type={"text"} ref="course7" placeholder="course7" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input nameref="course8"  type={"text"} ref="course8" placeholder="course8" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input nameref="course9"  type={"text"} ref="course9" placeholder="course9" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input nameref="course10" type={"text"} ref="course10" placeholder="course10" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input nameref="course11" type={"text"} ref="course11" placeholder="course11" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input nameref="course12" type={"text"} ref="course12" placeholder="course12" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
                             </div>
                             <div>
                                 Y4 H1
-                                <input name="course13" type={"text"} ref="course13" placeholder="course13" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course14" type={"text"} ref="course14" placeholder="course14" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course15" type={"text"} ref="course15" placeholder="course15" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course16" type={"text"} ref="course16" placeholder="course16" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course17" type={"text"} ref="course17" placeholder="course17" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course18" type={"text"} ref="course18" placeholder="course18" style={{width:"100px", margin:"5px"}}/>
+                                <input name="course13" type={"text"} ref="course13" placeholder="course13" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course14" type={"text"} ref="course14" placeholder="course14" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course15" type={"text"} ref="course15" placeholder="course15" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course16" type={"text"} ref="course16" placeholder="course16" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course17" type={"text"} ref="course17" placeholder="course17" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course18" type={"text"} ref="course18" placeholder="course18" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
                             </div>
                             <div>
                                 Y4 H2
-                                <input name="course19" type={"text"} ref="course19" placeholder="course19" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course20" type={"text"} ref="course20" placeholder="course20" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course21" type={"text"} ref="course21" placeholder="course21" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course22" type={"text"} ref="course22" placeholder="course22" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course23" type={"text"} ref="course23" placeholder="course23" style={{width:"100px", margin:"5px"}}/>
-                                <input name="course24" type={"text"} ref="course24" placeholder="course24" style={{width:"100px", margin:"5px"}}/>
+                                <input name="course19" type={"text"} ref="course19" placeholder="course19" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course20" type={"text"} ref="course20" placeholder="course20" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course21" type={"text"} ref="course21" placeholder="course21" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course22" type={"text"} ref="course22" placeholder="course22" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course23" type={"text"} ref="course23" placeholder="course23" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
+                                <input name="course24" type={"text"} ref="course24" placeholder="course24" style={{width:"100px", margin:"5px", borderRadius: "5px", border: "2px solid #c4c4c4"}}/>
                             </div>
-                            <div>Comments<input type={"text"} ref="comment" placeholder="comments" style={{width:"600px"}} /></div>
+                            <div>Comments<input type={"text"} ref="comment" placeholder="comments" style={{width:"600px", borderRadius: "5px", border: "2px solid #c4c4c4"}} /></div>
                             <br></br>
                             <button type="submit">Submit</button>
                         </form>
