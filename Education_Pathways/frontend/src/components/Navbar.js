7691e3e6df582f42fdbf4c82e16c86b86bd8fb61
Rainey <59759137+raineyfu@users.noreply.github.com>
2022-11-14 01:35:44 -0500

    Revert "Add Website Change Color Theme"
    This reverts commit c628120838b332ed3dd8bb10614f5e5a53332f76.

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect} from "react";
+import React, { Component } from "react";
 import "./css/navbar.css";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "./img/logo.png";
@@ -17,11 +17,6 @@ import CourseDescriptionPage from "./CourseDescription";
 // import SignUp from './SignUp'
 import SearchResultDisplay from "./ResultDisplay";
 import TopTemplatedPathway from "./TopTemplatedPathways";
-import Modal from "react-bootstrap/Modal";
-
-
-
-
 function CourseDescription(props) {
   let query = useQuery();
   return <CourseDescriptionPage code={query.get("code")} />;
@@ -39,8 +34,6 @@ export default class NavbarComp extends Component {
     this.state = {
       username: localStorage.getItem("username"),
       login: false,
-      modalOpen: false,
-      currentColor: "blue",
     };
   }
 
@@ -53,69 +46,10 @@ export default class NavbarComp extends Component {
   logOut = () => {
     localStorage.setItem("username", "");
     this.setState({ username: "" });
-  }
-
-  openModal = () => {
-    this.setState({modalOpen: true});
-  }
-  
-  closeModal = () => {
-    this.setState({modalOpen: false});
-  }
-
-  changeColor = (e, newColor) => {
-    e.preventDefault();
-    this.setState({currentColor: newColor});
-    let backgroundColor = "#1c3e6f";
-    let textColor = "gray";
-
-    if (newColor === "gray") {
-      backgroundColor = "gray";
-      textColor = "black";
-    }
-
-    if (newColor === "pink") {
-      backgroundColor = "pink";
-      textColor = "gray";
-    }
-
-    if (newColor === "red") {
-      backgroundColor = "#ff9999";
-      textColor = "black";
-    }
-
-    if (newColor === "green") {
-      backgroundColor = "#99ff99";
-      textColor = "black";
-    }
-
-    var ele = document.getElementsByClassName("navbar");
-    for (let i = 0; i < ele.length; i++) {
-      ele[i].style.backgroundColor = backgroundColor;
-    }
-
-    var ele = document.getElementsByClassName("submit-button");
-    for (let i = 0; i < ele.length; i++) {
-      ele[i].style.backgroundColor = backgroundColor;
-    }
-
-    var all = document.getElementsByTagName("*");
-    for (let i = 0; i < all.length; i++) {
-      all[i].style.color = textColor;
-    }
-
-    var links = document.getElementsByClassName("search-result-item");
-    for (let i = 0; i < links.length; i++) {
-      links[i].style.color = "#0d6efd";
-    }
-    document.getElementById("modalHeading").style.backgroundColor = backgroundColor;
-    document.getElementById("modalText").style.color = backgroundColor;
-  }
+  };
 
   render() {
     return (
-
-      <>
       <Router>
         <div>
           <Navbar bg="myBlue" variant="dark" sticky="top" expand="lg">
@@ -138,8 +72,6 @@ export default class NavbarComp extends Component {
                 {/* <Nav.Link href="/search" style={{ color: "white", display: "inline" }}>
                   Search
                 </Nav.Link> */}
-                
-                <button onClick={this.openModal} class="themeButton">Customize Theme</button>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
@@ -190,7 +122,7 @@ export default class NavbarComp extends Component {
               path="/courseDetails/:code"
               render={(props) => <CourseDescriptionPage {...props} />}
             ></Route>
-            <Route path="/templated_pathways" onClick={event => this.changeColor(event, this.state.currentColor)}>
+            <Route path="/templated_pathways">
               <TemplatedPathway/>
               <TopTemplatedPathway/>
             </Route>
@@ -200,25 +132,6 @@ export default class NavbarComp extends Component {
           </Switch>
         </div>
       </Router>
-      <div>
-      <Modal show={this.state.modalOpen} onHide={this.closeModal}>
-            <Modal.Header closeButton id="modalHeading">
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div id="modalText">Themes: </div>
-                <br />
-                <div> 
-                  <span onClick={event => this.changeColor(event, "#1c3e6f")} class="dot" id="blue"></span> 
-                  <span onClick={event => this.changeColor(event, "gray")} class="dot" id="black"> </span> 
-                  <span onClick={event => this.changeColor(event, "pink")} class="dot" id="pink"> </span>
-                  <span onClick={event => this.changeColor(event, "red")} class="dot" id="red"> </span>
-                  <span onClick={event => this.changeColor(event, "green")} class="dot" id="green"> </span>
-                </div>
-            </Modal.Body>
-          </Modal>
-      </div>
-      </>
     );
   }
 }
